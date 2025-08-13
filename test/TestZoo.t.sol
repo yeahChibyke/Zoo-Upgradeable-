980b505e68a744407ce7c5f66684cd6203c68bc9
yeahChibyke <chibykepride@gmail.com>
2025-08-14 00:01:49 +0100

    upgrade to jungle logic needs to be fixed before tests

---
+++ test/TestZoo.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {Zoo} from "../src/Zoo.sol";
+
+contract TestZoo is Test {
+    Zoo zoo;
+    address zooKeeper;
+}
